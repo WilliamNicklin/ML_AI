123686019096e83b209081c8b349f2ca04850a92
Will_Nick <64916592+WilliamNicklin@users.noreply.github.com>
2023-05-12 11:18:23 +0100

    Update helper_functions.py

--- helper_functions.py
+++ helper_functions.py
@@ -2,16 +2,7 @@ import tensorflow as tf
 
 # Create a function to import an image and resize it to be able to be used with our model
 def load_and_prep_image(filename, img_shape=224, scale=True):
-  """
-  Reads in an image from filename, turns it into a tensor and reshapes into
-  (224, 224, 3).
 
-  Parameters
-  ----------
-  filename (str): string filename of target image
-  img_shape (int): size to resize target image to, default 224
-  scale (bool): whether to scale pixel values to range(0, 1), default True
-  """
   # Read in the image
   img = tf.io.read_file(filename)
   # Decode it into a tensor
